9d6cc470866be2ee59b20570e4a3760dc1adac2e
Ramudu <uramudu257@gmail.com>
2019-09-06 15:43:48 +0530

    first commit

--- Radha1/settings.py
+++ Radha1/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'Radha1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Radha',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'School'),
     }
 }
+
 
 
 
